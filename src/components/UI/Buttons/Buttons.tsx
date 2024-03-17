09dfc3a5b4b942e3f7d24200fd9d719366cae8cb
Aleksey Kosourov <aleksey.kosourov@decathlon.com>
2024-03-17 20:39:27 +0300

    fix ui styles

--- src/components/UI/Buttons/Buttons.tsx
+++ src/components/UI/Buttons/Buttons.tsx
@@ -2,6 +2,7 @@ import { Dispatch } from '@reduxjs/toolkit';
 import { FC } from 'react';
 import { handleAddToCart, handleRemoveFromCart } from '../../../features/Cart/actions';
 import { Product } from '../../../features/Products/types';
+import { FaPlus, FaMinus } from 'react-icons/fa';
 
 import './Buttons.css';
 
@@ -14,8 +15,8 @@ const Buttons:FC<Props> = ({dispatch, product}) => {
   return (
     <div className='buttons'>
       <div className='buttons'>
-          <button className='button button-add' onClick={() => handleAddToCart(dispatch, product)}>+</button>
-          <button className='button button-remove' onClick={() => handleRemoveFromCart(dispatch, product.id)}>-</button>
+          <button className='button button-add' onClick={() => handleAddToCart(dispatch, product)}><FaPlus /></button>
+          <button className='button button-remove' onClick={() => handleRemoveFromCart(dispatch, product.id)}><FaMinus /></button>
       </div>
       
     </div>
