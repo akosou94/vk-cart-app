4dc877be73b11d29124ac7826fcb6542286336ab
Aleksey Kosourov <aleksey.kosourov@decathlon.com>
2024-03-17 23:04:09 +0300

    add api.ts baseUrl

--- src/features/Products/api.ts
+++ src/features/Products/api.ts
@@ -1,7 +1,8 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
+const baseUrl = 'https://fakestoreapi.com'
 
 export const fetchProductsAsync = createAsyncThunk('products/fetchProductsAsync', async () => {
-  const response = await fetch('https://fakestoreapi.com/products');
+  const response = await fetch(`${baseUrl}/products`);
   const data = await response.json();
   return data;
 });
